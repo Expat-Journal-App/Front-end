4c418f645d0cfd2a3aacf840e04019c7eb7ee9d9
niklasbec <niklasbeckerr@gmail.com>
2019-11-18 17:16:47 +0100

    I added router tags to the app then I created the formik form for the createnewstory page, I also created a route to the url we declared together!

--- src/components/createNewStory.js
+++ src/components/createNewStory.js
@@ -1,12 +1,85 @@
 import React from 'react';
-
+import { Form, Field, withFormik, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
 
 function CreateNewStory() {
   return (
     <div>
-      
+      <Form>
+          <ErrorMessage name='title' render={msg => <div className='error'>{msg}</div>} />
+          <label>Title
+            <Field type='text' name='title' placeholder='Enter a title!' />
+          </label>
+
+          <ErrorMessage name='tripDate' render={msg => <div className='error'>{msg}</div>} />
+          <label>Trip-Date
+            <Field type='text' name='tripDate' placeholder='Enter the date of your trip!' />
+          </label>
+
+          <ErrorMessage name='country' render={msg => <div className='error'>{msg}</div>} />
+          <label>Country
+            <Field type='text' name='country' placeholder='Enter a country!' />
+          </label>
+
+          <ErrorMessage name='city' render={msg => <div className='error'>{msg}</div>} />
+          <label>City
+            <Field type='text' name='city' placeholder='Enter a city!' />
+          </label>
+
+          <ErrorMessage name='image1' render={msg => <div className='error'>{msg}</div>} />
+          <label>Image 1
+            <Field type='text' name='image1' placeholder='Paste image link here!' />
+          </label>
+
+          <ErrorMessage name='image2' render={msg => <div className='error'>{msg}</div>} />
+          <label>Image 2
+            <Field type='text' name='image2' placeholder='Paste image link here!' />
+          </label>
+
+          <ErrorMessage name='image3' render={msg => <div className='error'>{msg}</div>} />
+          <label>Image 3
+            <Field type='text' name='image3' placeholder='Paste image link here!' />
+          </label>
+
+
+      </Form>
     </div>
   );
 }
 
-export default CreateNewStory;
+const formikCreateNewStory = withFormik ({
+    mapPropsToValues() {
+        return {
+            title: '',
+            tripDate: '',
+            country: '',
+            city: '',
+            image1: '',
+            image2: '',
+            image3: ''
+
+        }
+    },
+
+    validationSchema: Yup.object().shape({
+        title: Yup.string().required('Please enter a title!'),
+        tripDate: Yup.string().required('Please enter a trip date!'),
+        country: Yup.string().required('Please enter a country!'),
+        city: Yup.string().required('Please enter a city!'),
+        image1: Yup.string().required('Please enter a valid image URL!'),
+        image2: Yup.string().required('Please enter a valid image URL!'),
+        image3: Yup.string().required('Please enter a valid image URL!'),
+        
+    }),
+
+    handleSubmit(values, tools) {
+        console.log(values);
+        console.log(tools);
+
+        //AXIOS CALL WILL GO HERE
+
+    }
+})(CreateNewStory)
+
+export default formikCreateNewStory
