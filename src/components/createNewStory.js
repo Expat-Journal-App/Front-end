0e35d9a23d817ef3489328bb1b988e91841b394c
niklasbec <niklasbeckerr@gmail.com>
2019-11-18 15:58:10 +0100

    created react component file structure

---
+++ src/components/createNewStory.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function CreateNewStory() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default CreateNewStory;
---
+++ src/components/storyList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function StoryList() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default StoryList;
