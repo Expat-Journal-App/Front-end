2b3da271098d1cb2e57c119a5cb7d9c09badde5d
niklasbec <niklasbeckerr@gmail.com>
2019-11-19 09:01:02 +0100

    played around some more with the axios and rendered the first item to stories

--- src/components/storyList.js
+++ src/components/storyList.js
@@ -1,13 +1,14 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios'
 
 function StoryList() {
+    const [stories, setStories] = useState([])
 
     useEffect(() => {
     
         axios.get('http://localhost:4400/api/stories')
         .then(response => {
-          console.log(response);
+         setStories(response.data);
         })
         .catch(error => {
           console.log(error);
@@ -16,7 +17,15 @@ function StoryList() {
 
   return (
     <div>
-      
+      {stories.map(story =>(
+          <div className='story' key={story.id}>
+            <h1>{story.title}</h1>
+            <p>Date of trip: {story.date_trip}</p>
+            <p>Story created: {story.created_at}</p>
+            <h2>Story:</h2>
+            <p>{story.story}</p>
+          </div>
+      ))}
     </div>
   );
 }
