f36da31066264e20d721c6d5d7923d672ef61452
niklasbec <niklasbeckerr@gmail.com>
2019-11-19 08:47:22 +0100

    added simple axios call to storyList to test the api

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import "./App.css";
 import { Route, Link } from "react-router-dom";
 import formikCreateNewStory from "./components/createNewStory";
 import NavBar from "./components/NavBar";
-
 import StoryPage from './components/storyPage'
+import StoryList from "./components/storyList";
 
 function App() {
   return (
     <>
       <NavBar />
       <Route exact path="/stories/add" component={formikCreateNewStory} />
+      <Route exact path='/stories' component={StoryList} />
     </>
   );
 }
--- src/components/storyList.js
+++ src/components/storyList.js
@@ -1,7 +1,19 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import axios from 'axios'
 
 function StoryList() {
+
+    useEffect(() => {
+    
+        axios.get('http://localhost:4400/api/stories')
+        .then(response => {
+          console.log(response);
+        })
+        .catch(error => {
+          console.log(error);
+        })
+      }, []);
+
   return (
     <div>
       
