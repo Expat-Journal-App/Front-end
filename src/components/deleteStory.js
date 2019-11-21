9ee661070045bb84a2c3d87ee9a1e2b07ccb88bb
niklasbec <niklasbeckerr@gmail.com>
2019-11-21 13:14:05 +0100

    had to change the formik against since it was bugging, working on a submit bug I cant quite figure out

--- src/components/deleteStory.js
+++ src/components/deleteStory.js
@@ -10,3 +10,15 @@ function DeleteStory() {
 }
 
 export default DeleteStory;
+
+  /*
+    REMOVE AXIOS
+    onClick={
+        axios.delete(`https://morning-sea-62543.herokuapp.com/api/stories/${props.match.params.id}`)
+          .then(response => {
+            console.log(response.data);
+          })
+          .catch((error) => {
+            console.log(error);
+          })
+      } */
